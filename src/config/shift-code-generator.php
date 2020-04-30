336f29bab536c81e0578402de965f93fa471b093
techguy <jobvinny@1993>
2020-05-01 01:47:24 +0300

    :wrench: Changing configuration files.

---
+++ src/config/shift-code-generator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * -----------------------------------
+ * Define the application parameters
+ * ------------------------------------
+ * This includes the name and the year you
+ * would like the codes to be generated from
+ * ---------------------------------------------
+ */
+return [
+    'name' => 'Shiftech Africa',
+];
