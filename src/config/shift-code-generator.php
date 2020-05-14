2a4c035aae240db07ed220dcbecaf93faa4a8787
dev-techguy <dev.techguy@gmail.com>
2020-05-14 12:18:02 +0300

    :wrench: Changing configuration files.

--- src/config/shift-code-generator.php
+++ src/config/shift-code-generator.php
@@ -7,6 +7,11 @@
  * would like the codes to be generated from
  * ---------------------------------------------
  */
+
+use Carbon\Carbon;
+
 return [
-    'name' => 'Shiftech Africa',
+    'name' => env('APP_NAME', 'Shiftech Africa'),
+    'year' => Carbon::create(env('YEAR_OF_START', date('Y')), now()->month, now()->day),
+    'use_db' => env('USE_DATABASE', false),
 ];
